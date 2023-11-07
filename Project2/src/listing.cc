5f0b433b191f9b99c9f121b95826445d30aa5a6a
cjkresho1 <37028133+cjkresho1@users.noreply.github.com>
2023-11-07 09:55:17 -0500

    Base Project 2 files, with my Project 1 additions.

---
+++ Project2/src/listing.cc
@@ -0,0 +1,72 @@
+// Compiler Theory and Design
+// Dr. Duane J. Jarc
+
+// This file contains the bodies of the functions that produces the compilation
+// listing
+
+#include <cstdio>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+#include "listing.h"
+
+static int lineNumber;
+static queue<string> errorQueue;
+static int totalErrors = 0;
+static int lexicalErrors = 0;
+static int syntaxErrors = 0;
+static int semanticErrors = 0;
+
+static void displayErrors();
+
+void firstLine()
+{
+	lineNumber = 1;
+	printf("\n%4d  ",lineNumber);
+}
+
+void nextLine()
+{
+	displayErrors();
+	lineNumber++;
+	printf("%4d  ",lineNumber);
+}
+
+int lastLine()
+{
+	printf("\r");
+	displayErrors();
+	printf("     \n");
+
+	if (totalErrors == 0) {
+		printf("Compiled Successfully\n");
+	}
+	else {
+		printf("Lexical Errors %d\n", lexicalErrors);
+		printf("Syntax Errors %d\n", syntaxErrors);
+		printf("Semantic Errors %d\n", semanticErrors);
+	}
+
+	return totalErrors;
+}
+    
+void appendError(ErrorCategories errorCategory, string message)
+{
+	string messages[] = { "Lexical Error, Invalid Character ", "",
+		"Semantic Error, ", "Semantic Error, Duplicate Identifier: ",
+		"Semantic Error, Undeclared " };
+
+	errorQueue.push(messages[errorCategory] + message);
+	totalErrors++;
+	lexicalErrors++;
+}
+
+void displayErrors()
+{
+	while (errorQueue.size() != 0) {
+			printf("%s\n", errorQueue.front().c_str());
+			errorQueue.pop();
+	}
+}
---
+++ Project2/src/tokens.h
@@ -0,0 +1,9 @@
+// Compiler Theory and Design
+// Dr. Duane J. Jarc
+
+// This file contains the enumerated type definition for tokens
+
+enum Tokens {RELOP = 256, ADDOP, MULOP, REMOP, EXPOP, ANDOP, OROP, NOTOP,
+	ARROW, BEGIN_, BOOLEAN, CASE, ELSE, END, ENDCASE, ENDIF, ENDREDUCE,
+	FUNCTION, IF, INTEGER, IS, OTHERS, REAL, REDUCE, RETURNS, THEN, WHEN, IDENTIFIER,
+	INT_LITERAL, BOOL_LITERAL, REAL_LITERAL};
